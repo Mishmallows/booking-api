1ae7be2b29ae988528c44cacc34988445ce74436
Mishka Angela <125977103+Mishmallows@users.noreply.github.com>
2025-09-01 19:52:26 +0800

    Refactor webhook route and update comments

--- routes/webhook.js
+++ routes/webhook.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/webhook/calcom', (req, res) => {
+router.post('/calcom', (req, res) => {
   const { triggerEvent, payload } = req.body;
 
   console.log(`🔔 Webhook received: ${triggerEvent}`);
   console.log('Payload:', payload);
 
-  // TODO: Save booking info to database or in-memory cache
+  // TODO: Save booking info to database or cache
   // Example: store bookingUid, title, equipmentType, startTime, endTime
 
   res.status(200).send({ received: true });
